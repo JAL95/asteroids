4f83cbced68e756c8de51bbce5ff29209a9b3f04
JAL95 <jaredlamberson@gmail.com>
2025-02-14 19:44:40 -0500

    Added player, fixed clock

--- main.py
+++ main.py
@@ -2,13 +2,21 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
+from circleshape import CircleShape
 from constants import *
 
 def main():
     pygame.init()
+
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -16,9 +24,14 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-
+        
         screen.fill((0, 0, 0))
+
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
