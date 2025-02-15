6ec001113c49610e6c143d9dfb1687805a8db8af
JAL95 <jaredlamberson@gmail.com>
2025-02-15 00:39:12 -0500

    created a game over on player collision with asteroid

--- circleshape.py
+++ circleshape.py
@@ -12,6 +12,9 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
+    def detect_collision(self, other):
+        return self.position.distance_to(other.position) < self.radius + other.radius
+
     def draw(self, screen):
         # must override
         pass
